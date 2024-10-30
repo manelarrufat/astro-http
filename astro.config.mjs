9a2dbefd8bf7966fd67be003cfa13aec39410f75
Manel Arrufat <manelarrufat@gmail.com>
2024-10-30 16:57:20 +0100

    Afegit cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,19 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
   output: 'hybrid',
+  adapter: cloudflare()
 
-  adapter: node({
-    mode: 'standalone',
-  }),
+
+  // output: 'hybrid',
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
 });
