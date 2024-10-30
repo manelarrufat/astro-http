5ced0315246ad92f1643b35a2291a91f50a8d1bb
Manel Arrufat <manelarrufat@gmail.com>
2024-10-30 17:08:46 +0100

    modificada configuració server

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
-  output: 'hybrid',
+  output: 'server',
   adapter: cloudflare()
 
 
