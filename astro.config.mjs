80a4a9bf9d03c2b75f0b6d45c3d6263457093e8c
Manel Arrufat <manelarrufat@gmail.com>
2024-11-13 11:25:01 +0100

    modificada configuracio

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://561c4e09.astro-http-1u1.pages.dev/',
   integrations: [mdx(), sitemap(), db()],
   output: 'hybrid',
   adapter: cloudflare(),
