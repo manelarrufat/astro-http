6b89ff8ec5448441d86be0c7938ce739f752c5fe
Manel Arrufat <manelarrufat@gmail.com>
2024-11-13 11:18:32 +0100

    Canviada configuracio a Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,18 +6,20 @@ import sitemap from '@astrojs/sitemap';
 
 import node from '@astrojs/node';
 import db from '@astrojs/db';
-// import cloudflare from '@astrojs/cloudflare';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), db()],
-  // output: 'hybrid',
-  // adapter: cloudflare()
-
-
   output: 'hybrid',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare(),
+
+
+  // output: 'hybrid',
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
 });
+
+
